d02892cb84fbee0b0474826059a551f9caaefd88
Nikola Djikic <nikoladjikic22@gmail.com>
2020-06-23 23:22:31 +0200

    Add validation to register form and matching changes to the backend logic

--- front/src/app/register/register.component.ts
+++ front/src/app/register/register.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
 import { RegisterService } from '../service/register.service';
+import { confirmedValidator } from '../helpers/confirmed.validator';
+
 
 @Component({
   selector: 'app-register',
@@ -9,18 +11,42 @@ import { RegisterService } from '../service/register.service';
 })
 export class RegisterComponent implements OnInit {
 
+  submitted = false;
+
   registerForm=this.fb.group({
-      ime:[''],
-      prezime:[''],
-      email:[''],
-      username:[''],
-      passwordHash:['']
+      ime:['', [Validators.required, Validators.minLength(2), Validators.pattern(/^[A-Z]/),]],
+      prezime:['', [Validators.required, Validators.minLength(2), Validators.pattern(/^[A-Z]/),]],
+      email:['', [Validators.required, Validators.pattern(/^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/)]],
+      username:['', [Validators.required, Validators.pattern(/^[A-z0-9]{5,}$/)]],
+      passwordHash:['', [Validators.required, Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,}$/)]],
+      confirmPassword:['', [Validators.required]]
+  },{
+      validators: confirmedValidator('passwordHash', 'confirmPassword')
   })
+
   constructor(private fb:FormBuilder,private registerService:RegisterService) { }
 
   ngOnInit(): void {
   }
-  onSubmit(){
-    this.registerService.registerUser(this.registerForm.value).subscribe();
+  async onSubmit(){
+    this.submitted = true;
+    if(this.registerForm.invalid) {
+      return;
+    }
+    await this.registerService.registerUser(this.registerForm.value).subscribe(data =>{
+      if (data.statusCode === -7000) {
+        const email = this.registerForm.get('email');
+        email.setErrors({'notUnique': true});
+      }
+      if (data.statusCode === -7001) {
+        const username = this.registerForm.get('username');
+        username.setErrors({'notUnique': true});
+      }
+    });
   }
+
+  onReset() {
+    this.submitted = false;
+    this.registerForm.reset();
+}
 }
