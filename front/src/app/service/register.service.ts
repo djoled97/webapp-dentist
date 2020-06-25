0745f3c83c124d7ee0effeabccfe91582ae1f538
Nikola Djikic <nikoladjikic22@gmail.com>
2020-06-25 19:53:05 +0200

    Fix register page routing to login page

--- front/src/app/service/register.service.ts
+++ front/src/app/service/register.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpHeaders, HttpClient } from '@angular/common/http';
 import { User } from '../models/User';
+import { tap } from 'rxjs/operators';
+import { Router } from '@angular/router';
 const httpOptions = {
   headers: new HttpHeaders({
     'Content-Type':  'application/json' ,
@@ -12,10 +14,15 @@ const httpOptions = {
 })
 export class RegisterService {
 
-  constructor(private http:HttpClient) { }
+  constructor(private http:HttpClient, private router: Router) { }
 
   registerUser(user:User){
-     return this.http.put<any>("http://localhost:3000/auth/register",user,httpOptions)
+     return this.http.put<any>("http://localhost:3000/auth/register",user,httpOptions).pipe(
+       tap(res =>{
+          this.router.navigate(['../login']);
+       })
+     )
+     
   }
 
 }
